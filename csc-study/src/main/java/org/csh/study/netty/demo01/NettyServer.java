8cc21fc9a32a03fa34d58a71fa747d77fe907958
1CSH1 <602685158@qq.com>
2018-04-15 23:05:04 +0800

    add

---
+++ csc-study/src/main/java/org/csh/study/netty/demo01/NettyServer.java
@@ -0,0 +1,39 @@
+package org.csh.study.netty.demo01;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+
+    public static void main(String[] args) {
+        int port = 1000;
+        if (null != args && args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        new NettyServer().bind(port);
+    }
+
+    private void bind(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new ChildChannelHandler());
+        try {
+            ChannelFuture future = serverBootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
